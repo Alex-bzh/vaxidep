f0ca058967829276b77607177a88f6fdb207fae6
Alexandre Roulois <alexandre.roulois@u-paris.fr>
2021-02-07 10:07:12 +0100

    Add component Timeline

--- app/main.js
+++ app/main.js
@@ -22,6 +22,16 @@ const app = Vue.createApp({
     },
     methods: {
         /*
+        *   When a day is picked up on the timeline,
+        *   it triggers a new GeoJSON layer.
+        */
+        changeLayer: function(date) {
+            // A call to the method initGeoJSON() in the frCovidMapCmpnt
+            // to set a new layer.
+            this.$refs.map.initGeoJSON(this.departments, date);
+            this.dateToDisplay = date;
+        },
+        /*
         *   Fetches the data
         */
         fetchData() {
@@ -32,7 +42,35 @@ const app = Vue.createApp({
                 this.dateToDisplay = data.date;
                 // Initializes the GeoJSON layer
                 this.$refs.map.initGeoJSON(data.features, data.date);
+                this.$refs.timeline.listDates([2020, 11, 27], data.date);
             });
+        },
+        /*
+        *   When a day is picked up on the timeline,
+        *   it also triggers a new Chart.
+        */
+        setLimit(limit) {
+            // Removes the old chart.
+            this.$refs.chart.removeChart();
+            // Sets a new Chart with accurate metrics.
+            this.$refs.chart.getMetrics(this.zone, limit);
+        },
+        /*
+        *   A simple way to show all the data in the chart.
+        */
+        showAll() {
+            this.zoomDept(this.dateToDisplay, 'france');
+        },
+        /*
+        *   Zoom to a specific department
+        */
+        zoomDept(limit, dept) {
+            // The zone is fixed to the department
+            this.zone = dept;
+            // Removes the old chart.
+            this.$refs.chart.removeChart();
+            // Sets a new Chart with accurate metrics.
+            this.$refs.chart.getMetrics(dept, limit);
         }
     }
 });
