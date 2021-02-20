8ec0b10d7937b5ae417ee590475d6cd9640aa1dc
Alexandre Roulois <alexandre.roulois@u-paris.fr>
2021-02-20 17:02:38 +0100

    Remove timeline

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             departments: null,
-            dateToDisplay: moment(),
+            dateToDisplay: moment().format('LL'),
             zone: 'france'
         }
     },
@@ -12,25 +12,7 @@ const app = Vue.createApp({
         */
         this.fetchData();
     },
-    computed: {
-        /*
-        *   Converts the date to display into a Moment format.
-        */
-        displayDate() {
-            return moment(this.dateToDisplay).format('LL');
-        }
-    },
     methods: {
-        /*
-        *   When a day is picked up on the timeline,
-        *   it triggers a new GeoJSON layer.
-        */
-        changeLayer: function(date) {
-            // A call to the method initGeoJSON() in the frCovidMapCmpnt
-            // to set a new layer.
-            this.$refs.map.initGeoJSON(this.departments, date);
-            this.dateToDisplay = date;
-        },
         /*
         *   Fetches the data
         */
@@ -39,38 +21,20 @@ const app = Vue.createApp({
             .then(stream => stream.json())
             .then(data => {
                 this.departments = data.features;
-                this.dateToDisplay = data.date;
                 // Initializes the GeoJSON layer
-                this.$refs.map.initGeoJSON(data.features, data.date);
-                this.$refs.timeline.listDates([2020, 11, 27], data.date);
+                this.$refs.map.initGeoJSON(data.features);
             });
-        },
-        /*
-        *   When a day is picked up on the timeline,
-        *   it also triggers a new Chart.
-        */
-        setLimit(limit) {
-            // Removes the old chart.
-            this.$refs.chart.removeChart();
-            // Sets a new Chart with accurate metrics.
-            this.$refs.chart.getMetrics(this.zone, limit);
-        },
-        /*
-        *   A simple way to show all the data in the chart.
-        */
-        showAll() {
-            this.zoomDept(this.dateToDisplay, 'france');
         },
         /*
         *   Zoom to a specific department
         */
-        zoomDept(limit, dept) {
+        zoomDept(dept) {
             // The zone is fixed to the department
             this.zone = dept;
             // Removes the old chart.
-            this.$refs.chart.removeChart();
+            //this.$refs.chart.removeChart();
             // Sets a new Chart with accurate metrics.
-            this.$refs.chart.getMetrics(dept, limit);
+            //this.$refs.chart.getMetrics(dept, limit);
         }
     }
 });
